9f99bd325a94516125753153b375002778dd8a30
Rubin0902 <73993026+Rubin0902@users.noreply.github.com>
2020-12-19 16:09:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(displayWidth,displayHeight);
+    
+
+	engine = Engine.create();
+	world = engine.world;
+	ground = new Ground(displayWidth/2,600, displayWidth, 20);
+	red1 = new Rect((displayWidth/2+displayWidth/3),540,10,100);
+	red3 = new Rect((displayWidth/2+displayWidth/3)+150,540,10,100);
+	red2 = new Rect((displayWidth/2+displayWidth/3)+75,585,150,10);
+	ball = new Ball(200, 100, 70, 70);
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display();
+  ball.display();
+  red2.display();
+  red1.display();
+  red3.display();
+  drawSprites();
+ 
+}
+function keyPressed()
+{
+	Matter.Body.applyForce(ball.body,ball.body.position,{x:600,y:-190})
+}
+
+
